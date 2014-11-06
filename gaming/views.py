3f31ff257e3be48a4543e9d4cb8cddcd11588bd9
ajain <ajain@castlighthealth.com>
2014-11-06 17:09:46 +0530

    changes in view

--- gaming/views.py
+++ gaming/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-#from gaming.models import MyModel   # Import the model classes we just wrote.
+from gaming.models import User_Images   # Import the model classes we just wrote.
 
 def index(request):
-    #latest_question_list = MyModel.objects.all().order_by('-pub_date')[:5]
+    user_images = User_Images.objects.all()
     #context = {'latest_question_list': latest_question_list}
-    context = {'latest_question_list': "adasdasdas"}
+    context = {'user_images': user_images}
     return render(request, 'gaming/index.html', context)
