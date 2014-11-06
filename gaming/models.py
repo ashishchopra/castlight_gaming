8e59f1cfa2a958a54e62b3742ce6e62cc8b77ac6
jyotismitat <jyotismitat@lmiller-x1.ch.int>
2014-11-06 13:37:26 +0530

    models created

--- gaming/models.py
+++ gaming/models.py
@@ -3,5 +3,23 @@ from django.db import models
 # Create your models here.
 class MyModel (model.Model):
 	id=models.varchar(max_length=50)
-	image_name=models.varchar(max_length=50)
-        user_id=models.varchar(max_length=50)
+	name=models.varchar(max_length=50)
+        #user_id=models.varchar(max_length=50)
+        class Meta:
+             db_table = 'Users'
+
+class GameAttempt(meodel.Model):
+       id=models.varchar(max_length=50)
+       user_id=models.varchar(max_length=50)
+       game_id=models.varchar(max_length=50)
+       attempt_date=models.datetime(max_length=50)
+       score=models.varchar(max_length=50)
+       
+class User_Images(model.Model):
+       id=models.varchar(max_length=50)
+       user_id=models.varchar(max_length=50)
+       image_path=models.varchar(max_length=50)
+
+class Game(model.Model):
+       id=models.varchar(max_length=50)
+       game_name=models.varchar(max_length=50)
