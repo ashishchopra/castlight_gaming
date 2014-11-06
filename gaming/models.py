71692fa3536de3c68c834e9bdd98828eaf6227b1
ajain <ajain@castlighthealth.com>
2014-11-06 13:39:33 +0530

    database config

--- gaming/models.py
+++ gaming/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class MyModel (model.Model):
-	id=models.varchar(max_length=50)
-	image_name=models.varchar(max_length=50)
-        user_id=models.varchar(max_length=50)
+class MyModel (models.Model):
+  id=models.varchar(max_length=50)
+  image_name=models.varchar(max_length=50)
+  user_id=models.varchar(max_length=50)
--- gaming/views.py
+++ gaming/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+#from gaming.models import MyModel   # Import the model classes we just wrote.
+
+def index(request):
+    #latest_question_list = MyModel.objects.all().order_by('-pub_date')[:5]
+    #context = {'latest_question_list': latest_question_list}
+    context = {'latest_question_list': "adasdasdas"}
+    return render(request, 'gaming/index.html', context)
